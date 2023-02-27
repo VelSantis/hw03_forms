1cba910710962d9404bc2017ab3c5bbf28b54fb5
Андрей К <steel28_07@mail.ru>
2023-02-27 13:11:43 +0300

    5

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,11 +6,8 @@ from posts import views
 app_name = 'posts'
 
 urlpatterns = [
-    # Главная страница
     path('', views.index, name='index'),
-    # Профайл пользователя
     path('profile/<str:username>/', views.profile, name='profile'),
-    # Просмотр записи
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('create/', views.post_create, name='post_create'),
