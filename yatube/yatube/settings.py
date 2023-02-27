3fda6986eb5543f02c29e6dc1fdefd7f0941dbb7
Андрей К <steel28_07@mail.ru>
2023-02-27 13:15:12 +0300

    6

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -123,8 +123,6 @@ LOGIN_URL = 'users:login'
 
 LOGIN_REDIRECT_URL = 'posts:index'
 
-#LOGOUT_REDIRECT_URL = 'posts:index'
-
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 # указываем директорию, в которую будут складываться файлы писем
